914eb84e5d56d5caadb4dcccfbf43ce8f7b4fd36
1ureka <summerheadquarters001@gmail.com>
2025-11-23 09:26:39 +0800

    新增圖片牆偏好設定狀態管理，支持模式與欄位大小的設定

---
+++ src/webviews/imageWall/imageWallPreference.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+type ImageWallPreferenceState = {
+  mode: "standard" | "quilted" | "woven" | "masonry";
+  columnSize: "s" | "m" | "l";
+};
+
+const imageWallPreferenceStore = create<ImageWallPreferenceState>(() => ({
+  mode: "masonry",
+  columnSize: "m",
+}));
+
+const setImageWallPreference = (preference: Partial<ImageWallPreferenceState>) => {
+  imageWallPreferenceStore.setState((state) => ({ ...state, ...preference }));
+};
+
+export { imageWallPreferenceStore, setImageWallPreference, type ImageWallPreferenceState };
