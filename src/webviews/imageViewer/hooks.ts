09a0c9df3035fa2f51f29167fddffd235218a185
1ureka <summerheadquarters001@gmail.com>
2025-11-25 06:21:03 +0800

    為 ImageViewer 新增 useDecodeImage 鉤子以解碼圖片並回傳狀態，接管原生行為

--- src/webviews/imageViewer/hooks.ts
+++ src/webviews/imageViewer/hooks.ts
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { postMessageToExtension } from "../utils/vscodeApi";
 
+/** 收到訊息後啟動取色工具 */
 const useEyeDropper = () => {
   useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
@@ -24,4 +25,48 @@ const useEyeDropper = () => {
   }, []);
 };
 
-export { useEyeDropper };
+/** 延遲指定毫秒數 */
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/** 使用 async/await 解碼圖片，如果解碼失敗，將進行重試直到成功。*/
+async function decode(image: HTMLImageElement, attempt = 0): Promise<void> {
+  try {
+    await image.decode();
+  } catch (error) {
+    if (attempt < 1000) {
+      await decode(image, attempt + 1);
+    } else {
+      throw new Error(`Reached maximum decode attempts (1000)`);
+    }
+  }
+}
+
+/** 解碼圖片並回傳解碼後的圖片 URL 及解碼狀態 */
+const useDecodeImage = (src: string | null): [string | null, boolean] => {
+  const [state, setState] = useState(false);
+  const [_src, setSrc] = useState<string | null>(null);
+  const timeStamp = useRef<number | null>(null);
+
+  const decoding = (url: string, createAt: number) => {
+    const img = new Image();
+    img.src = url;
+
+    (async () => {
+      await delay(250);
+      await decode(img);
+      if (createAt !== timeStamp.current) return;
+      setSrc(url);
+      setState(true);
+    })();
+  };
+
+  useEffect(() => {
+    timeStamp.current = Date.now();
+    setState(false);
+    if (src) decoding(src, timeStamp.current);
+  }, [src]);
+
+  return [_src, state];
+};
+
+export { useEyeDropper, useDecodeImage };
