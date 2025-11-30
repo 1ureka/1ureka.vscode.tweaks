debe94e9b1843afc8bed47ab6caa560fe049f7f4
1ureka <summerheadquarters001@gmail.com>
2025-11-30 16:49:08 +0800

    新增 defer 函數以支援 Promise 的解析與拒絕邏輯

---
+++ src/utils/promise.ts
@@ -0,0 +1,13 @@
+function defer<T>() {
+  let resolve!: (value: T | PromiseLike<T>) => void;
+  let reject!: (reason?: any) => void;
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
+}
+
+export { defer };
