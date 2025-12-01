93e2a9248b430332c9921861ec6b513f84afe624
1ureka <summerheadquarters001@gmail.com>
2025-12-02 03:24:09 +0800

    新增訊息處理機制，強化類型安全

--- src/utils/message_client.ts
+++ src/utils/message_client.ts
@@ -1,3 +1,6 @@
+import { defer, type Promised } from "@/utils";
+import type { API, InvokeResponseMessage, ForwardCommandMessage } from "@/utils/message_host";
+
 /**
  * 獲取初始數據（從 HTML 中的 script 標籤提取）
  */
@@ -21,10 +24,60 @@ function getInitialData<T = any>(): T | null {
 const vscode = acquireVsCodeApi();
 
 /**
- * 向擴展發送消息
+ * webview 希望調用擴展主機的處理函式時所發送的消息結構
  */
-function postMessageToExtension(message: any) {
+type InvokeMessage = {
+  type: "1ureka.invoke";
+  requestId: string;
+  handlerId: string;
+  params: any;
+};
+
+/**
+ * 調用擴展主機處理函式並獲取結果，使用時須導入由擴展主機定義的處理函式型別就能讓前後端型別自動對齊
+ * @example
+ * // 假設在擴展主機中有一個處理函式
+ * async function myHandler(params: { id: string }): Promise<{ name: string; age: number }>
+ * type MyAPI = { id: "myHandler"; handler: typeof myHandler };
+ *
+ * // 那麼在前端調用時可以這樣寫
+ * import type { MyAPI } from "path/to/extension/host/api"; // 注意要加 type 關鍵字，因為前端無法使用實際的擴展主機代碼
+ * const resultInWebview = await invoke<MyAPI>("myHandler", { id: "123" });
+ * // resultInWebview 的類型會自動推斷為 { name: string; age: number }
+ */
+function invoke<T extends API>(id: T["id"], params: Parameters<T["handler"]>[0]): Promised<T["handler"]> {
+  const { promise, resolve } = defer<Awaited<ReturnType<T["handler"]>>>();
+  const requestId = crypto.randomUUID();
+
+  const message: InvokeMessage = { type: "1ureka.invoke", requestId, handlerId: id, params };
   vscode.postMessage(message);
+
+  const handleMessage = (event: MessageEvent<InvokeResponseMessage>) => {
+    const message = event.data;
+    if (message.type === "1ureka.invoke.response" && message.requestId === requestId && message.handlerId === id) {
+      window.removeEventListener("message", handleMessage);
+      resolve(message.result);
+    }
+  };
+
+  window.addEventListener("message", handleMessage);
+  return promise;
 }
 
-export { postMessageToExtension, getInitialData };
+/**
+ * 處理來自擴展主機的命令消息
+ * 詳情請參考 src/utils/message_host.ts 中的 forwardCommandMessage 相關說明
+ */
+function onReceiveCommand<T extends API>(id: T["id"], handler: T["handler"], params: Parameters<T["handler"]>[0]) {
+  const handleMessage = (event: MessageEvent<ForwardCommandMessage>) => {
+    const message = event.data;
+    if (message.type === "1ureka.command" && message.action === id) {
+      handler(params);
+    }
+  };
+
+  window.addEventListener("message", handleMessage);
+}
+
+export { getInitialData, invoke, onReceiveCommand };
+export type { InvokeMessage };
