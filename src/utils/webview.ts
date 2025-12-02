3d45027b192f3c5acca9c2a89d8adde2be9f1110
1ureka <summerheadquarters001@gmail.com>
2025-12-02 15:22:47 +0800

    新增 createWebviewPanelManager 函數以管理 Webview Panel 的生命週期，並提供創建及獲取當前面板的功能

--- src/utils/webview.ts
+++ src/utils/webview.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { randomUUID } from "crypto";
 import type { OneOf } from "@/utils";
 
 /**
@@ -98,4 +99,40 @@ function createWebviewPanel<T>(params: CreateWebviewPanelParams<T>) {
   return panel;
 }
 
-export { createWebviewPanel };
+/**
+ * 創建一個 Webview Panel 管理器，用於管理多個 Webview Panel 的生命週期
+ */
+function createWebviewPanelManager(context: vscode.ExtensionContext) {
+  const panels = new Map<string, vscode.WebviewPanel>();
+
+  /**
+   * 創建一個新的 Webview Panel，會自動管理其生命週期
+   */
+  const create = <T>(params: CreateWebviewPanelParams<T>): vscode.WebviewPanel => {
+    const panel = createWebviewPanel(params);
+    const panelId = randomUUID();
+    panels.set(panelId, panel);
+
+    const disposeListener = panel.onDidDispose(() => {
+      panels.delete(panelId);
+      disposeListener.dispose();
+    });
+
+    context.subscriptions.push(panel, disposeListener);
+    return panel;
+  };
+
+  /**
+   * 獲取當前使用者正在互動的 Webview Panel
+   */
+  const getCurrent = (): vscode.WebviewPanel | null => {
+    for (const panel of panels.values()) {
+      if (panel.active) return panel;
+    }
+    return null;
+  };
+
+  return { create, getCurrent };
+}
+
+export { createWebviewPanelManager };
