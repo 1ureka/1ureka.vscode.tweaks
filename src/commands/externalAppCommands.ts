b9c814819586d3860d46e62b632e7f02a87e39d4
1ureka <summerheadquarters001@gmail.com>
2025-12-02 15:44:56 +0800

    外部應用程式和樣式注入命令改為使用 createCommandManager 函數

--- src/commands/externalAppCommands.ts
+++ src/commands/externalAppCommands.ts
@@ -1,22 +1,23 @@
 import * as vscode from "vscode";
 import { handleOpenApp, handleOpenFile } from "../handlers/externalAppHandlers";
+import { createCommandManager } from "@/utils/command";
 
 export function registerExternalAppCommands(context: vscode.ExtensionContext) {
-  const openBlenderCommand = vscode.commands.registerCommand("1ureka.openBlender", () => {
+  const commandManager = createCommandManager(context);
+
+  commandManager.register("1ureka.openBlender", () => {
     handleOpenApp("blender");
   });
 
-  const openPainterCommand = vscode.commands.registerCommand("1ureka.openPainter", () => {
+  commandManager.register("1ureka.openPainter", () => {
     handleOpenApp("painter");
   });
 
-  const openWithBlenderCommand = vscode.commands.registerCommand("1ureka.openWithBlender", (uri: vscode.Uri) => {
+  commandManager.register("1ureka.openWithBlender", (uri: vscode.Uri) => {
     handleOpenFile(uri);
   });
 
-  const openWithPainterCommand = vscode.commands.registerCommand("1ureka.openWithPainter", (uri: vscode.Uri) => {
+  commandManager.register("1ureka.openWithPainter", (uri: vscode.Uri) => {
     handleOpenFile(uri);
   });
-
-  context.subscriptions.push(openBlenderCommand, openWithBlenderCommand, openPainterCommand, openWithPainterCommand);
 }
--- src/utils/command.ts
+++ src/utils/command.ts
@@ -7,6 +7,7 @@ function createCommandManager(context: vscode.ExtensionContext) {
   /**
    * 註冊一個 VSCode 命令，自動管理其生命週期，不返回，確保外部代碼無法保有該命令的引用
    */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const register = (commandId: string, callback: (...args: any[]) => any) => {
     const command = vscode.commands.registerCommand(commandId, callback);
     context.subscriptions.push(command);
