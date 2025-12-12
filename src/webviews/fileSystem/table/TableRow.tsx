c54942092a99c9dbaca01c1485953d8712a11fb3
1ureka <summerheadquarters001@gmail.com>
2025-12-13 02:09:16 +0800

    refactor(table): 將用於給 row 提供剪貼簿視覺回饋的組件抽到獨立檔案

--- src/webviews/fileSystem/table/TableRow.tsx
+++ src/webviews/fileSystem/table/TableRow.tsx
@@ -1,78 +1,13 @@
 import React from "react";
-import { Box, ButtonBase } from "@mui/material";
+import { ButtonBase } from "@mui/material";
 import type { ButtonBaseProps, SxProps } from "@mui/material";
 
 import type { TableFields } from "./common";
 import { tableColumns, tableRowBaseSx } from "./common";
 import { TableIconCell, TableCell, TableEditingCell } from "./TableRowCell";
 
-import { useIsInClipboard } from "../data/clipboard";
 import { endRenaming } from "../data/action";
-
-/**
- * 用於定位徽章元素的樣式
- */
-const tableRowBadgeAnchorSx: SxProps = {
-  position: "absolute",
-  inset: "0 0 auto auto",
-  display: "grid",
-  placeItems: "center",
-};
-
-/**
- * 用於徽章本體的樣式
- */
-const tableRowBadgeSx: SxProps = {
-  position: "absolute",
-  borderRadius: 1,
-  bgcolor: "background.paper",
-  p: 0.25,
-  boxShadow: "0 2px 4px var(--vscode-widget-shadow)",
-  border: "1px solid",
-  borderColor: "tooltip.border",
-  display: "grid",
-  placeItems: "center",
-};
-
-/**
- * 用於呈現某 row 正在被複製或剪下的徽章
- */
-const TableRowBadge = () => (
-  <Box sx={{ ...tableRowBadgeAnchorSx, color: "text.secondary" }}>
-    <Box sx={tableRowBadgeSx}>
-      <i className="codicon codicon-copy" />
-    </Box>
-  </Box>
-);
-
-/**
- * 用於正在被複製或剪下的 row 的虛線邊框樣式
- */
-const tableRowClipboardBorderSx: SxProps = {
-  "& > rect": {
-    stroke: "var(--mui-palette-text-primary)",
-    strokeOpacity: 0.2,
-    strokeWidth: 1.5,
-    strokeDasharray: "14 8",
-    fill: "none",
-    animation: "dash 2s linear infinite",
-    rx: "var(--mui-shape-borderRadius)",
-    ry: "var(--mui-shape-borderRadius)",
-  },
-
-  "@keyframes dash": { to: { strokeDashoffset: -110 } },
-};
-
-/**
- * 用於給正在被複製或剪下的 row 提供視覺回饋
- */
-const TableRowClipboardBorder = () => (
-  <Box component="svg" preserveAspectRatio="none" width="100%" height="100%" sx={tableRowClipboardBorderSx}>
-    <rect width="calc(100%)" height="calc(100%)" />
-  </Box>
-);
-
-// ----------------------------------------------------------------------------
+import { TableRowClipboardDecorator } from "./TableRowDecorators";
 
 /**
  * 普通資料列組件的 props 型別
@@ -87,8 +22,6 @@ type TableRowProps = ButtonBaseProps & {
  * 用於呈現一個普通的資料列
  */
 const TableRow = ({ sx, row, isDraggable, isRenaming, ...props }: TableRowProps) => {
-  const isInClipboard = useIsInClipboard(row.filePath);
-
   const handleDragStart: React.DragEventHandler<HTMLButtonElement> = (e) => {
     const fileUrl = `file:///${row.filePath.replace(/\\/g, "/")}`;
     const filePath = row.filePath;
@@ -132,12 +65,7 @@ const TableRow = ({ sx, row, isDraggable, isRenaming, ...props }: TableRowProps)
         );
       })}
 
-      {isInClipboard && (
-        <Box sx={{ position: "absolute", inset: 0, pointerEvents: "none" }}>
-          <TableRowClipboardBorder />
-          <TableRowBadge />
-        </Box>
-      )}
+      <TableRowClipboardDecorator filePath={row.filePath} />
     </ButtonBase>
   );
 };
