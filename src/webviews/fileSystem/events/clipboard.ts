90c1915c9c4cd2c2f49fe9e1be513e43019683a8
1ureka <summerheadquarters001@gmail.com>
2025-12-19 18:28:05 +0800

    refactor(Clipboard): 更新剪貼簿功能，重命名函數並調整事件註冊邏輯

--- src/webviews/fileSystem/events/clipboard.ts
+++ src/webviews/fileSystem/events/clipboard.ts
@@ -1,12 +1,12 @@
-import { invokeClipboardPaste, setClipboard } from "@@/fileSystem/action/clipboard";
+import { readClipboard, writeClipboard } from "@@/fileSystem/action/clipboard";
 
 /**
  * 註冊剪貼簿事件
  */
 const registerClipboardEvents = () => {
-  window.addEventListener("copy", setClipboard);
-  window.addEventListener("cut", setClipboard);
-  window.addEventListener("paste", invokeClipboardPaste);
+  window.addEventListener("copy", writeClipboard);
+  window.addEventListener("cut", writeClipboard);
+  window.addEventListener("paste", readClipboard);
 };
 
 export { registerClipboardEvents };
