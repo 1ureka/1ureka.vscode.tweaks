7d6f292b7d02f9e3712bec5199d03a0921689823
1ureka <summerheadquarters001@gmail.com>
2025-12-27 14:59:43 +0800

    fix: 修復當出現 overflowX 時，LoadingDisplay 並沒有佔滿寬度的問題

--- src/webview-explorer/index.tsx
+++ src/webview-explorer/index.tsx
@@ -4,7 +4,7 @@ import { readInitData } from "@explorer/store/init";
 import { setupDependencyChain } from "@explorer/store/dependency";
 import { registerAllShortcuts } from "@explorer/action/shortcuts";
 
-import { Box } from "@mui/material";
+import { Box, type SxProps } from "@mui/material";
 import { LoadingDisplay } from "@explorer/layout/LoadingDisplay";
 import { NavigationPanels } from "@explorer/layout/NavigationPanels";
 import { NavigationBar } from "@explorer/layout/NavigationBar";
@@ -13,24 +13,51 @@ import { TableBody } from "@explorer/layout-table/TableBody";
 import { ActionBar } from "@explorer/layout/ActionBar";
 import { ImageGrid } from "@explorer/layout-grid/ImageGrid";
 
-const Container = ({ children }: { children: React.ReactNode }) => (
-  <Box sx={{ position: "relative", height: "100dvh", width: "100dvw", overflow: "hidden", overflowX: "auto" }}>
-    <Box sx={{ minWidth: 850, display: "grid", gridTemplateColumns: "270px 1fr", height: 1 }}>{children}</Box>
-    <LoadingDisplay />
-  </Box>
-);
+const appClassName = {
+  scrollContainer: "explorer-scroll-container",
+  contentContainer: "explorer-content-container",
+  layout: "explorer-layout",
+};
+
+const panelsWidth = 270;
+
+const containerSx: SxProps = {
+  position: "relative",
+  height: "100dvh",
+  width: "100dvw",
+  overflow: "hidden",
+  overflowX: "auto",
+
+  [`& .${appClassName.contentContainer}`]: {
+    position: "relative",
+    height: 1,
+    minWidth: 850,
+  },
+
+  [`& .${appClassName.layout}`]: {
+    display: "grid",
+    gridTemplateColumns: `${panelsWidth}px 1fr`,
+    height: 1,
+    width: 1,
+  },
+};
 
 const App = () => (
-  <Container>
-    <NavigationPanels />
-    <Box sx={{ p: 1, display: "flex", flexDirection: "column", height: 1, minHeight: 0 }}>
-      <NavigationBar />
-      <TableHead />
-      <TableBody />
-      <ActionBar />
-      <ImageGrid />
-    </Box>
-  </Container>
+  <Box className={appClassName.scrollContainer} sx={containerSx}>
+    <div className={appClassName.contentContainer}>
+      <div className={appClassName.layout}>
+        <NavigationPanels />
+        <Box sx={{ p: 1, display: "flex", flexDirection: "column", height: 1, minHeight: 0 }}>
+          <NavigationBar />
+          <TableHead />
+          <TableBody />
+          <ActionBar />
+          <ImageGrid />
+        </Box>
+      </div>
+      <LoadingDisplay />
+    </div>
+  </Box>
 );
 
 startReactApp({
